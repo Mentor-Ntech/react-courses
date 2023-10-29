22eb7a44bb523e99241b1e82aebb2e689b6dc5ff
Mentor-Ntech <oloyedeaderayo11@gmail.com>
2023-10-29 08:09:34 +0100

    useEffect hook

---
+++ hook-useeffect/src/AddItem.js
@@ -0,0 +1,36 @@
+import React, { useRef } from 'react'
+import {FaPlus} from 'react-icons/fa'
+//import { useRef } from 'react'
+
+const AddItem = ({newItem, setNewItem, handleSubmit}) => {
+
+    const inputRef = useRef()
+
+  return (
+    <form className='addForm' onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor='addItem'>AddItem</label>
+        <input 
+        
+        autoFocus
+        ref={inputRef}
+        id='addItem'
+        type='text'
+        placeholder='addItem'
+        required
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+        />
+        <button 
+        type='submit'
+        aria-label='Add Item'
+        onClick={() => inputRef.current.focus()}
+        >
+            <FaPlus 
+            />
+        </button>
+        
+    </form>
+  )
+}
+
+export default AddItem
---
+++ hook-useeffect/src/App.js
@@ -0,0 +1,74 @@
+import Header from "./Header";
+import Search from "./Search";
+import Content from "./Content";
+import Footer from "./Footer";
+import { useEffect, useState } from "react";
+import AddItem from "./AddItem";
+import EventListenerComponent from "./EventListenerComponent";
+
+function App() {
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem("shoppinglist")) || []);
+
+  const [newItem, setNewItem] = useState("");
+
+  const [search, setSearch] = useState("");
+
+  //useEffect
+    
+
+  useEffect(() => {
+    localStorage.setItem("shoppinglist", JSON.stringify(items));
+  }, [items])
+
+
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setItems(listItems);
+  };
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem("");
+  };
+
+  return (
+    <div className="App">
+      <Header title="DltAfrica 3.0" />
+
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+
+      <Search search={search} setSearch={setSearch} />
+      <Content
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+
+      {/* < EventListenerComponent/> */}
+      <Footer length={items.length} />
+    </div>
+  );
+}
+
+export default App;
